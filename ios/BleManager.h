2be972d47629c60bc1da0720f2b34f26d6344d1e
Mathis Gardon <mathis.gardon@gmail.com>
2023-03-07 16:13:39 +0100

    WIP

--- ios/BleManager.h
+++ ios/BleManager.h
@@ -1,5 +1,5 @@
-#import "React/RCTBridgeModule.h"
-#import "React/RCTEventEmitter.h"
+#import <React/RCTBridgeModule.h>
+#import <React/RCTEventEmitter.h>
 #import <CoreBluetooth/CoreBluetooth.h>
 
 
